68a2f3bdcec4603f84bd44ae99a121107fbc6662
koushik nag shuvo <60665707+koushiknagshuvo@users.noreply.github.com>
2023-11-10 18:37:43 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/', [UserController::class, 'index']);
